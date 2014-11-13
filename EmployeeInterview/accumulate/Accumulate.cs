eaa3845b93629151d9eb6f0f507f36114f36c56f
Rob Prouse <rob@prouse.org>
2014-11-13 13:20:38 -0500

    Cleaned the tests

--- EmployeeInterview/accumulate/Accumulate.cs
+++ EmployeeInterview/accumulate/Accumulate.cs
@@ -3,11 +3,4 @@ using System.Collections.Generic;
 
 public static class AccumulateExtensions
 {
-    public static IEnumerable<T2> Accumulate<T1, T2>(this IEnumerable<T1> source, Func<T1, T2> operation )
-    {
-        foreach (var item in source)
-        {
-            yield return operation(item);
-        }
-    }
 }
--- EmployeeInterview/prime-factors/PrimeFactors.cs
+++ EmployeeInterview/prime-factors/PrimeFactors.cs
@@ -4,12 +4,6 @@ public static class PrimeFactors
 {
     public static IEnumerable<long> For( long i )
     {
-        int factor = 2;
-        while ( i > 1 )
-        {
-            while ( i % factor != 0 ) factor++;
-            i /= factor;
-            yield return factor;
-        }
+        return new long[] { };
     }
 }
